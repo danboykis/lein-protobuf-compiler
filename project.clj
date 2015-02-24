8d968abb1fecf636949af3b8d5ab7521b975e090
Michael Nygard <mtnygard@gmail.com>
2015-02-24 06:39:58 -0600

    Version 0.4.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-protobuf-minimal "0.4.3"
+(defproject lein-protobuf-minimal "0.4.4-SNAPSHOT"
   :description       "Leiningen plugin for compiling protocol buffers."
   :license           {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url               "https://github.com/mtnygard/lein-protobuf-minimal"
   :dependencies      [[fs         "1.2.0"]
                       [conch      "0.2.0"]
-                      [leinjacker "0.4.0"]]
+                      [leinjacker "0.4.2"]]
   :eval-in-leiningen true
   :checksum-deps     true)
